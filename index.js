6bebb0e38264f026f7353a9364744dd15042d8be
Luiz <luizmarquesfernando037@gmail.com>
2026-07-08 21:07:19 -0300

    aula contas matematica

--- index.js
+++ index.js
@@ -1,42 +1,42 @@
 import express from "express"
- import bancoDeDados from "./repository/index.js"
 const app = express()
-// server ou app
-//Configura uma rota para o metodo HTTP GET na raiz ("/") do aplicativo
-// app.get("/api/v1/somar", (req, res) => {
- 
-// console.log(req.query)
- 
-//     const num1 = Number(req.query.num1)
-//     const num2 =  Number(req.query.num2)
-//     const resultado = num1 + num2
-//     //Retorna uma resposta JSON contendo uma mensagem "Hello Word"
-//     res.send({ resultado })
-// })
- 
-app.get("/api/pessoa/:id", (req, res) => {
-    const id = req.params.id
-    const pessoa = bancoDeDados.find(it => it.id == id)
-    if (!pessoa){
-        res.send({ message: "Pessoa não encontrada" })
-        return
-    }
- 
-    res.send({pessoa})
-})
-app.get("/api/pessoa", (req, res) => {
-    const { id, name } = req.query
-    if(!id || !name) {
-        res.send({ message: "Favor informar id e name" })
-        return
-    }
-    bancoDeDados.push({id, name})
-    res.send({ message: "Pessoa criada com sucesso" })
 
+//somar 
+app.get("/api/v1/somar", (req, res) => {
+    //const num1 = req.query.num1
+    //const num2 = req.query.num2
+    const { num1, num2 } = req.query
+    const resultado = Number(num1) + Number(num2)
+
+    res.status(200).send({ message: resultado })
+    //res.send({ message: resultado})
 })
- console.log(bancoDeDados)
- 
-//Inicia o servidor Express na porta 3000 e define uma função de retorno de chamada
+
+
+//subtrair
+app.get("/api/v1/subtrair", (req, res) => {
+    const {num1, num2 } = req.query 
+    const resultado = Number(num1) - Number(num2)
+
+    res.status(200).send({ message: resultado })
+})
+
+
+//mult
+app.get("/api/v1/multiplicar", (req, res) => {
+    const {num1, num2 } = req.query
+    const resultado = Number(num1) * Number(num2)
+
+    res.status(200).send({ message: resultado })
+})
+//div
+app.get("/api/v1/divisao", (req, res) => {
+    const {num1, num2 } = req.query
+    const resultado = Number(num1) / Number(num2)
+
+    res.status(200).send({ message: resultado })
+})
+
 app.listen(3000, () => {
     //Exbice uma mensagem no console quando o servidor estiver ativo.
     console.log("servidor escutando na porta 3000")
